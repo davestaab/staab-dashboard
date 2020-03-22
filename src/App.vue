73784dc16c2e02c1b7e8d22e9874731b5181cc9c
Dave Staab <davestaab@gmail.com>
2020-03-22 18:14:55 -0400

    adding some css classes to make the dashboard grid

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="grid grid-cols-2 gap-4 h-screen p-4">
+      <div
+        class="bg-red-300 flex items-center justify-center border-red-500 border rounded-lg"
+      >
+        Calendar
+      </div>
+      <div
+        class="bg-orange-300 flex items-center justify-center border-red-500 border rounded-lg"
+      >
+        List
+      </div>
+      <div
+        class="bg-yellow-300 flex items-center justify-center border-red-500 border rounded-lg"
+      >
+        Pictures
+      </div>
+      <div
+        class="bg-green-300 flex items-center justify-center border-red-500 border rounded-lg"
+      >
+        Weather
+      </div>
+      <div
+        class="bg-blue-300 flex items-center justify-center border-red-500 border rounded-lg"
+      >
+        Quote
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -18,6 +46,10 @@ export default Vue.extend({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+html,
+body {
+  height: 100%;
 }
 </style>
