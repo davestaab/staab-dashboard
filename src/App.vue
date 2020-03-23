20beeed00894f4c6bd3615dc66437059224e8708
Dave Staab <davestaab@gmail.com>
2020-03-22 22:21:36 -0400

    initial grid layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <div class="grid grid-cols-2 gap-4 h-screen p-4">
+  <div id="app" class="flex">
+    <div class="grid grid-cols-12 grid-row-12 gap-4 p-4 flex-grow">
       <div
-        class="bg-red-300 flex items-center justify-center border-red-500 border rounded-lg"
+        class="row-span-5 col-span-9 bg-red-300 flex items-center justify-center border-red-700 border rounded-lg"
       >
         Calendar
       </div>
       <div
-        class="bg-orange-300 flex items-center justify-center border-red-500 border rounded-lg"
+        class="row-span-11 col-span-3 bg-orange-300 flex items-center justify-center border-orange-700 border rounded-lg"
       >
         List
       </div>
       <div
-        class="bg-yellow-300 flex items-center justify-center border-red-500 border rounded-lg"
+        class="row-span-5 col-span-6 bg-yellow-300 flex items-center justify-center border-yellow-700 border rounded-lg"
       >
         Pictures
       </div>
       <div
-        class="bg-green-300 flex items-center justify-center border-red-500 border rounded-lg"
+        class="row-span-6 col-span-3 bg-purple-300 flex items-center justify-center border-purple-700 border rounded-lg"
+      >
+        Meals
+      </div>
+      <div
+        class="row-span-1 col-span-6 bg-green-300 flex items-center justify-center border-green-700 border rounded-lg"
       >
         Weather
       </div>
       <div
-        class="bg-blue-300 flex items-center justify-center border-red-500 border rounded-lg"
+        class="col-span-12 bg-blue-300 flex items-center justify-center border-blue-700 border rounded-lg"
       >
         Quote
       </div>
@@ -51,5 +56,8 @@ export default Vue.extend({
 html,
 body {
   height: 100%;
+}
+.row-span-11 {
+  grid-row: span 11 / span 11;
 }
 </style>
