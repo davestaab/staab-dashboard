625ed3c85451bc5731c719a57caea97ee3f36b50
Dave Staab <davestaab@gmail.com>
2020-03-25 22:11:22 -0400

    embed google calendar in dashboard

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,56 @@
   <div id="app" class="flex">
     <div class="grid grid-cols-12 grid-row-12 gap-4 p-4 flex-grow">
       <div
-        class="row-span-5 col-span-9 bg-red-300 flex items-center justify-center border-red-700 border rounded-lg"
+        class="row-span-5 col-span-9 bg-white flex items-center justify-center border-red-700 border rounded-lg"
       >
-        Calendar
+        <iframe
+          src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=bGJpbmJiYTBucDk4NnU0NHA3ZTN1cGpmNmNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23E67C73&amp;showTz=0&amp;showCalendars=0&amp;showTabs=0&amp;showPrint=0&amp;showTitle=0&amp;showNav=0&amp;showDate=0&amp;mode=WEEK"
+          style="border:solid 1px transparent"
+          width="1200"
+          height="400"
+          frameborder="0"
+          scrolling="no"
+        ></iframe>
       </div>
       <div
         class="row-span-11 col-span-3 bg-orange-300 flex items-center justify-center border-orange-700 border rounded-lg"
       >
-        List
+        <div>List</div>
       </div>
       <div
         class="row-span-5 col-span-6 bg-yellow-300 flex items-center justify-center border-yellow-700 border rounded-lg"
       >
-        Pictures
+        <div>Pictures</div>
       </div>
       <div
         class="row-span-6 col-span-3 bg-purple-300 flex items-center justify-center border-purple-700 border rounded-lg"
       >
-        Meals
+        <div>
+          Meals
+        </div>
       </div>
       <div
         class="row-span-1 col-span-6 bg-green-300 flex items-center justify-center border-green-700 border rounded-lg"
       >
-        Weather
+        <div>
+          Weather
+        </div>
       </div>
       <div
         class="col-span-12 bg-blue-300 flex items-center justify-center border-blue-700 border rounded-lg"
       >
-        Quote
+        <div>
+          Quote
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-
+import Vue from 'vue';
 export default Vue.extend({
-  name: "App",
+  name: 'App',
   components: {}
 });
 </script>
