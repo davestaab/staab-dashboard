75affbb5b6e5d5e6606a827abf1b50b381aee389
Dave Staab <davestaab@gmail.com>
2020-04-10 15:54:09 -0400

    changed calendar to be month, removed some cells from the grid

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app" class="flex">
-    <div class="grid grid-cols-12 grid-row-12 gap-4 p-4 flex-grow">
+  <div id="app">
+    <div class="grid grid-cols-12 grid-row-12 gap-4 p-4 flex-grow h-full">
       <div
         class="row-span-5 col-span-9 bg-white flex items-center justify-center border-red-700 border rounded-lg"
       >
         <iframe
-          src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=bGJpbmJiYTBucDk4NnU0NHA3ZTN1cGpmNmNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23E67C73&amp;showTz=0&amp;showCalendars=0&amp;showTabs=0&amp;showPrint=0&amp;showTitle=0&amp;showNav=0&amp;showDate=0&amp;mode=WEEK"
+          src="https://calendar.google.com/calendar/embed?height=500&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=bGJpbmJiYTBucDk4NnU0NHA3ZTN1cGpmNmNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23E67C73&amp;showTz=0&amp;showCalendars=0&amp;showTabs=0&amp;showPrint=0&amp;showTitle=0&amp;showNav=0&amp;showDate=0&amp;mode=MONTH"
           style="border:solid 1px transparent"
-          width="1200"
-          height="400"
+          width="1400"
+          height="500"
           frameborder="0"
           scrolling="no"
         ></iframe>
@@ -19,23 +19,9 @@
         <item-list :items="itemLists"></item-list>
       </div>
       <div
-        class="row-span-5 col-span-6 bg-yellow-300 flex items-center justify-center border-yellow-700 border rounded-lg"
+        class="row-span-6 col-span-9 bg-yellow-300 flex flex-column items-center justify-center border-yellow-700 border rounded-lg"
       >
-        <div>Pictures</div>
-      </div>
-      <div
-        class="row-span-6 col-span-3 bg-purple-300 flex items-center justify-center border-purple-700 border rounded-lg"
-      >
-        <div>
-          Meals
-        </div>
-      </div>
-      <div
-        class="row-span-1 col-span-6 bg-green-300 flex items-center justify-center border-green-700 border rounded-lg"
-      >
-        <div>
-          Weather
-        </div>
+        <div class="flex-grow">Pictures</div>
       </div>
       <div
         class="col-span-12 bg-blue-300 flex items-center justify-center border-blue-700 border rounded-lg"
@@ -82,8 +68,5 @@ export default defineComponent({
 html,
 body {
   height: 100%;
-}
-.row-span-11 {
-  grid-row: span 11 / span 11;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      gridRow: {
+        'span-7': 'span 7 / span 7',
+        'span-8': 'span 8 / span 8',
+        'span-9': 'span 9 / span 9',
+        'span-10': 'span 10 / span 10',
+        'span-11': 'span 11 / span 11',
+        'span-12': 'span 12 / span 12'
+      }
+    }
   },
   variants: {},
   plugins: []
