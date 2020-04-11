caca44c697809544ba52686d162098ffbdc0f251
Dave Staab <davestaab@gmail.com>
2020-04-11 15:14:06 -0400

    added meals to dashboard list

--- src/data/itemLists.ts
+++ src/data/itemLists.ts
@@ -25,5 +25,21 @@ export default [
         complete: false
       }
     ]
+  },
+  {
+    title: 'Meals',
+    items: [
+      { name: 'Chicken cordon blue pasta', complete: false },
+      { name: 'Whole chicken', complete: false },
+      { name: 'Shredded pork sandwiches', complete: false },
+      { name: 'Steak fajitas', complete: false },
+      { name: 'Beef and broccoli', complete: false },
+      { name: 'Crescent roll sandwiches', complete: false },
+      { name: 'Chicken burritos', complete: false },
+      { name: 'Ham, rolls, eggs, potatoes, green beans', complete: false },
+      { name: 'Keto pizza casserole', complete: false },
+      { name: 'Food truck', complete: false },
+      { name: 'Meatballs', complete: false }
+    ]
   }
 ] as ItemLists;
