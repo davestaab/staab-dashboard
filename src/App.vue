4bfc17a792abe8ec6e14ef69394941aea5366be6
Dave Staab <davestaab@gmail.com>
2020-05-04 22:32:22 -0400

    removing pictures for now

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,13 @@
     </div>
     <div class="grid grid-cols-12 grid-row-12 gap-4 p-4 flex-grow h-full">
       <div
-        class="row-span-5 col-span-9 bg-white flex items-center justify-center border-red-700 border rounded-lg"
+        class="row-span-11 col-span-9 bg-white flex items-center justify-center border-red-700 border rounded-lg"
       >
         <iframe
-          src="https://calendar.google.com/calendar/embed?height=500&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=bGJpbmJiYTBucDk4NnU0NHA3ZTN1cGpmNmNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23E67C73&amp;showTz=0&amp;showCalendars=0&amp;showTabs=0&amp;showPrint=0&amp;showTitle=0&amp;showNav=0&amp;showDate=0&amp;mode=MONTH"
+          src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=bGJpbmJiYTBucDk4NnU0NHA3ZTN1cGpmNmNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23E67C73&amp;showTz=0&amp;showCalendars=0&amp;showTabs=0&amp;showPrint=0&amp;showTitle=0&amp;showNav=0&amp;showDate=0&amp;mode=MONTH"
           style="border:solid 1px transparent"
           width="1400"
-          height="500"
+          height="600"
           frameborder="0"
           scrolling="no"
         ></iframe>
@@ -20,11 +20,6 @@
         class="row-span-11 col-span-3 bg-orange-300 border-orange-700 border rounded-lg"
       >
         <item-list :items="itemLists"></item-list>
-      </div>
-      <div
-        class="row-span-6 col-span-9 bg-yellow-300 flex flex-column items-center justify-center border-yellow-700 border rounded-lg"
-      >
-        <div class="flex-grow">Pictures</div>
       </div>
       <div
         class="col-span-12 bg-blue-300 flex items-center justify-center border-blue-700 border rounded-lg"
